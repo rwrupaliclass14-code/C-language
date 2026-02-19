2296447e99f49178cdf0a02ba84191561eeb6da7
rwrupaliclass14-code <rwrupaliclass14@gmail.com>
2026-02-19 11:03:20 +0530

    Add files via upload

---
+++ day1/program2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+ //2.WAP to find a the answer given formula (x-y)2.
+void main()
+{
+	int x=2,y=5;
+	printf("%d", (x-y)*(x-y));
+}
---
+++ day1/program3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+//3.WAP to find a the answer given formula (x+y)3.
+void main()
+
+{
+	int x=2,y=4;
+	printf("%d",(x+y)*(x+y)*(x+y));
+}
